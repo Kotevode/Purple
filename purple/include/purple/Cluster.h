562e143feaa8e9d2da93f7826651f9c67967437a
Mark <kotevode@icloud.com>
2017-04-22 16:37:54 +0500

    No cluster_tdef

--- purple/include/purple/Cluster.h
+++ purple/include/purple/Cluster.h
@@ -6,13 +6,19 @@
 #define PURPLE_CLUSTER_H
 
 #include <stdio.h>
+#include <boost/mpi.hpp>
+#include <vector>
+#include <list>
+#include <numeric>
 #include "Job.h"
 #include "JobInfo.h"
 #include "Processor.h"
-#include <boost/mpi.hpp>
-#include <vector>
+#include "ResultInfo.h"
 
 namespace Purple {
+
+    using namespace std;
+    namespace mpi = boost::mpi;
 
     class Cluster {
 
@@ -22,8 +28,42 @@ namespace Purple {
         ~Cluster();
 
         template<class job_type, class result_type>
-        std::vector<result_type>
-        process(std::vector<job_type> &jobs, Processor<job_type, result_type> &processor) const;
+        vector<result_type> process(vector<job_type> &jobs, Processor<job_type, result_type> &processor) const {
+            vector<JobInfo> info(jobs.size());
+            int i = 0;
+            for_each(jobs.begin(), jobs.end(), [&](job_type &j) {
+                info[i] = JobInfo(j, i);
+                i++;
+            });
+            distribute(info);
+
+            int rank = communicator.rank();
+            int partial_count = accumulate(info.begin(), info.end(), 0, [rank](int sum, auto &j) -> int {
+                return sum + (j.get_node_number() == rank ? 1 : 0);
+            });
+            vector<ResultInfo<result_type> > partial_results(partial_count);
+
+            int j = 0;
+            for_each(info.begin(), info.end(), [&](auto &info) {
+                if (info.get_node_number() != rank)
+                    return;
+                partial_results[j] =
+                        ResultInfo<result_type>(processor.process(jobs[info.get_index()]), info.get_index());
+                j++;
+            });
+            MPI_Barrier(communicator);
+            vector<vector<ResultInfo<result_type> >> gathered(communicator.size());
+            mpi::gather(communicator, partial_results, gathered, 0);
+
+            vector<result_type> results(jobs.size());
+            for_each(gathered.begin(), gathered.end(), [&](auto &partial) {
+                for_each(partial.begin(), partial.end(), [&](auto &result_info) {
+                    results[result_info.get_index()] = std::move(result_info.get_result());
+                });
+            });
+
+            return results;
+        }
 
         const mpi::communicator &get_communicator() const;
 
@@ -35,7 +75,5 @@ namespace Purple {
     };
 
 }
-
-#include "cluster_tdef.cpp"
 
 #endif //PURPLE_CLUSTER_H
